a927df27a60409dc86ac64d6a9e584f190537b9b
tanayvarma123 <104054754+tanayvarma123@users.noreply.github.com>
2025-04-20 19:16:01 +0530

    Update fetch_data.py

--- Source/fetch_data.py
+++ Source/fetch_data.py
@@ -42,6 +42,7 @@ if 'PPIUS' in df.columns:
 # Set Date as index and interpolate
 df = df.set_index('Date')
 df.iloc[:, 1:] = df.iloc[:, 1:].interpolate(method='linear', inplace=False)
+df = df.ffill()
 
 # Save as CSV
 df.to_csv("Data/economic_data.csv")
